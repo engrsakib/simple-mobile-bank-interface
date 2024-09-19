7dd708b05ff63d2357afa1262b42fe0415f6dc87
engrsakib <snazmussakib01@gmail.com>
2024-09-19 12:46:15 +0600

    added money

--- script/home.js
+++ script/home.js
@@ -1,3 +1,17 @@
-const addAmount = document.getElementById('add-amount');
-const addPin = document.getElementById('add-pin');
-const addBtn = document.getElementById('button-add');
+const addAmount = document.getElementById("add-amount");
+const addPin = document.getElementById("add-pin");
+const addBtn = document.getElementById("button-add");
+
+// add money
+addBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  const addedTaka = addAmount.value;
+  const addPin = addPin.value;
+
+  if (addPin === "0123") {
+    
+  } else {
+    alert("something wrong please try again");
+  }
+});
